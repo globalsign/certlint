d2ee38af66d6a493abb6d02f99812df26681d348
Paul van Brouwershaven <paul@vanbrouwershaven.com>
2016-11-02 16:05:35 +0000

    Add required cache to tests

--- certlint_test.go
+++ certlint_test.go
@@ -4,9 +4,13 @@ import (
 	"fmt"
 	"io/ioutil"
 	"testing"
+
+	"github.com/golang/groupcache/lru"
 )
 
 func TestTestData(t *testing.T) {
+	var icaCache = lru.New(200)
+
 	// TODO: Check for specific errors per certificate to be sure we don't miss one
 	files, _ := ioutil.ReadDir("./testdata")
 	for _, f := range files {
@@ -14,7 +18,7 @@ func TestTestData(t *testing.T) {
 
 		der := getCertificate("./testdata/" + f.Name())
 		if len(der) > 0 {
-			result := do(der, nil, true, true)
+			result := do(icaCache, der, nil, true, true)
 			if len(result.Errors) == 0 {
 				t.Errorf("Exspected some errors, got %d in %s", len(result.Errors), f.Name())
 				continue
