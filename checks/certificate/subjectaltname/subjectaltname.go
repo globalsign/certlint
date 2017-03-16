403153d9a0a92bcb247d1e264deb3fbc3164beff
Paul van Brouwershaven <paul@vanbrouwershaven.com>
2017-03-16 13:27:07 +0000

    Check for whitespaces, not only start and end

--- checks/certificate/subjectaltname/subjectaltname.go
+++ checks/certificate/subjectaltname/subjectaltname.go
@@ -18,20 +18,24 @@ func init() {
 func Check(d *certdata.Data) *errors.Errors {
 	var e = errors.New(nil)
 
+	// TODO: Should we check against cross usage of certificate types (for example DV certificate with emial address)?
+
 	switch d.Type {
 	case "PS":
-		//if len(d.Cert.EmailAddresses) == 0 {
-		//	return fmt.Errorf("Certificate doesn't contain any subjectAltName")
-		//}
+		if len(d.Cert.EmailAddresses) == 0 {
+			e.Err("Certificate doesn't contain any subjectAltName")
+			return e
+		}
 		for _, s := range d.Cert.EmailAddresses {
-			if strings.HasPrefix(s, " ") || strings.HasSuffix(s, " ") {
-				e.Err("Certificate subjectAltName '%s' starts or ends with a whitespace", s)
+			if strings.Contains(s, " ") {
+				e.Err("Certificate subjectAltName '%s' contains a whitespace", s)
 			}
 		}
 
 	case "DV", "OV", "EV":
 		if len(d.Cert.DNSNames) == 0 && len(d.Cert.IPAddresses) == 0 {
 			e.Err("Certificate doesn't contain any subjectAltName")
+			return e
 		}
 
 		var cnInSan bool
@@ -39,8 +43,8 @@ func Check(d *certdata.Data) *errors.Errors {
 			if strings.EqualFold(d.Cert.Subject.CommonName, s) {
 				cnInSan = true
 			}
-			if strings.HasPrefix(s, " ") || strings.HasSuffix(s, " ") {
-				e.Err("Certificate subjectAltName '%s' starts or ends with a whitespace", s)
+			if strings.Contains(s, " ") {
+				e.Err("Certificate subjectAltName '%s' contains a whitespace", s)
 			}
 		}
 
