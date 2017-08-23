51f01f007ce5447332645b1e4d242304ea7072b3
Paul van Brouwershaven <paul@vanbrouwershaven.com>
2017-08-23 07:24:25 +0000

    Test case for Public Suffix checks

---
+++ checks/certificate/publicsuffix/publicsuffix_test.go
@@ -0,0 +1,31 @@
+package publicsuffix
+
+import (
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"fmt"
+	"testing"
+
+	"github.com/globalsign/certlint/certdata"
+)
+
+func TestPublicSuffix(t *testing.T) {
+	cd := &certdata.Data{
+		Cert: &x509.Certificate{
+			Subject: pkix.Name{
+				CommonName: "*.com",
+			},
+			DNSNames: []string{"*.com", "com", "*.co.uk", "gov.uk", "*.eu.com"},
+		},
+		Type: "DV",
+	}
+
+	e := Check(cd)
+	if len(e.List()) != 6 {
+		t.Errorf("Expected 6 errors, got %d", len(e.List()))
+	}
+
+	for _, err := range e.List() {
+		fmt.Println(err)
+	}
+}
