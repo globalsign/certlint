0feeed83a085651d23237215cf8ea9188555608b
Paul van Brouwershaven <paul@vanbrouwershaven.com>
2017-08-24 06:36:48 +0000

    Test case for isForbiddenString format check

---
+++ asn1/format_test.go
@@ -0,0 +1,25 @@
+package asn1
+
+import (
+	"testing"
+)
+
+func TestIsForbiddenString(t *testing.T) {
+	// Some forbidden characters to test
+	forbidden := []string{"-", "--", "_", "__", "-_", "- -", "?", "n/a", "N/A", ".", "+"}
+
+	for _, v := range forbidden {
+		if !isForbiddenString([]byte(v)) {
+			t.Errorf("Forbidden string passed check", v)
+		}
+	}
+
+	// These could be accepted values
+	accepted := []string{"OK+", "-OK"}
+
+	for _, v := range accepted {
+		if isForbiddenString([]byte(v)) {
+			t.Errorf("Accepted string did not pass check", v)
+		}
+	}
+}
